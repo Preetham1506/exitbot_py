021a71c66889cd7d277d35ccc8348f82aaea8943
Fireblazer1506 <85558545+Fireblazer1506@users.noreply.github.com>
2021-06-13 20:55:34 +0530

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -26,7 +26,8 @@ async def on_ready():
 async def preetham(ctx):
     await ctx.send("my developer")
 
-    @client.command(aliases=['tida'])
+    
+@client.command(aliases=['tida'])
 async def adit(ctx):
     await ctx.send("knows only 3 word - you, are and dumb")
 
