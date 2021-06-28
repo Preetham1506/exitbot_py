a30ca3793a7982ba2852d3cfa8e6ee5724ab9159
Fireblazer1506 <85558545+Fireblazer1506@users.noreply.github.com>
2021-06-28 17:24:05 +0530

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -13,7 +13,10 @@ async def on_command_error(ctx,error):
     elif isinstance(error,commands.CommandNotFound):
         await ctx.send("Invalid command")
 
-
+@client.event
+async def on_message(msg):
+    if "u r dumb" in msg.content:
+        await msg.add_reaction("<:aPES_NoU:617645169038196757>")
 @client.event
 async def on_ready():
     await client.change_presence(activity=discord.Game(name=f"on {len(client.guilds)} servers | ;command"))
