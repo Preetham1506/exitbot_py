7d5ae518c6e8a105d4a894f43fc4d14625018224
Fireblazer1506 <85558545+Fireblazer1506@users.noreply.github.com>
2021-06-28 18:12:49 +0530

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -24,27 +24,7 @@ async def on_message(msg):
         await msg.add_reaction("<:no_u:859041175771938827>")
     else:
         pass
-async def on_message(msg):
-    if msg.content == "i say die":
-        await msg.add_reaction("<:no_u:859041175771938827>")
-    else:
-        pass
-async def on_message(msg):
-    if msg.content == "fuck off":
-        await msg.add_reaction("<:no_u:859041175771938827>")
-    else:
-        pass
-@client.event
-async def on_message(msg):
-    if msg.content == "die":
-        await msg.add_reaction(":no_u:")
-    else:
-        pass
-async def on_message(msg):
-    if msg.content == "poop":
-        await msg.add_reaction("<:no_u:859041175771938827>")
-    else:
-        pass
+
 @client.command()
 @commands.has_permissions(ban_members=True)
 async def cu(ctx,cnum1,cnum2):
